f793cb1320e0b92ae88cf67a0bc4227cfd7d770e
FABIO <FABIO@DESKTOP-TUNODU8>
2018-03-24 23:19:29 +0100

    Done

--- FlussiDiControllo/src/InvertiStringa.java
+++ FlussiDiControllo/src/InvertiStringa.java
@@ -6,7 +6,7 @@ public class InvertiStringa {
 		String parola;
 		String inversa ="";
 		Scanner tastiera= new Scanner(System.in);
-		System.out.println("Inserisci una parola");
+		System.out.println("Inserisci una parola:");
 		parola= tastiera.nextLine();
 		for(int j=parola.length()-1;j>=0;j--)
 			inversa=inversa+parola.charAt(j);
