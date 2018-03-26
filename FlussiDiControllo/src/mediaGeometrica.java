50771e521a52fae9f59d14dea18a5f9f72e5cd15
FABIO <FABIO@DESKTOP-TUNODU8>
2018-03-26 09:35:43 +0200

    Done

---
+++ FlussiDiControllo/src/mediaGeometrica.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class mediaGeometrica {
+
+	public static void main(String[] args) {
+		double prodottoNumero = 1;
+		double numero;
+		int numeriInseriti;
+		Scanner tastiera= new Scanner(System.in);
+		System.out.println("Quanti numeri vuoi inserire?");
+		numeriInseriti= tastiera.nextInt();
+		for(int i=1; i<=numeriInseriti;i++){
+			System.out.println("Inserisci il "+i+" valore");
+			numero= tastiera.nextDouble();
+			if(numero>=0)
+				prodottoNumero= prodottoNumero*numero;
+			else{
+				while(!(numero>=0)){
+					System.out.println("Valore non valido, inserisci un valore positivo");
+					numero= tastiera.nextDouble();
+				}
+			}
+		}
+		double mediaGeometrica=Math.pow(prodottoNumero, 1.0/numeriInseriti);
+		System.out.println("La media geometrica e': "+ mediaGeometrica);
+	}
+
+}
